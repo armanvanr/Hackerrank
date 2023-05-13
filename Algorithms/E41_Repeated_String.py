cd6336eda286bc0fd058ff7efcae57b964841482
Venska Arman Nur Rosyidin <armanvenska@gmail.com>
2023-05-13 23:48:49 +0700

    algorithm-easy level-number 41-repeated string

--- Algorithms/E41_Repeated_String.py
+++ Algorithms/E41_Repeated_String.py
@@ -1,3 +1,10 @@
 def repeatedString(s, n):
-    
-    return 1
+    x = len(s)
+    r = n//x
+    p = n%x
+    res = s.count("a")*r + s[:p].count("a")
+    return res
+
+# x: length of string
+# r: number of repeating string
+# p: modulo result as last index
