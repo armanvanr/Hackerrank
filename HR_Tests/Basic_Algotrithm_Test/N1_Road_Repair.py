dd600122c61912a71c7dd25f9d32108d3a1944ab
Venska Arman Nur Rosyidin <armanvenska@gmail.com>
2023-05-13 23:36:52 +0700

    Problem Desc and Algorithm Explained

--- HR_Tests/Basic_Algotrithm_Test/N1_Road_Repair.py
+++ HR_Tests/Basic_Algotrithm_Test/N1_Road_Repair.py
@@ -8,3 +8,24 @@ def getMinCost(crew_id, job_id):
     return sum_dist
 
 print(getMinCost([5,9,4,3,8,2], [9,8,1,10,1,4]))
+
+# Problem Description
+# A number of points along the highway are in need of repair.
+# An equal number of crews are available, stationed at various points along the highway.
+# They must move along the highway to reach an assigned point.
+# Given that one crew must be assigned to each job,
+# what is the minimum total amount of distance traveled by all crews before they can begin work?
+
+# For example,
+# given crews at points {1,3,5} and required repairs at {3,5,7},
+# one possible minimum assignment would be
+# {1→ 3, 3 → 5, 5 → 7} for a total of 6 units traveled.
+
+
+# My algorithm:
+# Since any crew can go to any point,
+# sort the crew position ascending
+# and do the same for the job's points.
+# Calculate the absolute distance between crew and point for the same index respectfully,
+# e.g. distance between crew[0] and job[0], crew[1] and job[1], ... use index-based loop obviously
+# sum it all up
--- HR_Tests/Basic_Algotrithm_Test/N2_Max_Cost_Laptop_Count.py
+++ HR_Tests/Basic_Algotrithm_Test/N2_Max_Cost_Laptop_Count.py
@@ -15,3 +15,50 @@ def maxCost(cost, labels, dailyCount):
 
 # print(maxCost([2,5,3,11,1],["legal","illegal","legal","illegal","legal"],2))
 print(maxCost([0,3,2,3,4],["legal","legal","illegal","legal","legal"],1))
+
+# Problem Description:
+# A company manufactures a fixed number of laptops every day.
+# However, if some defect is encountered during the testing of a laptop,
+# it is labeled as "illegal" and is not counted in the laptop count of the day.
+# Given the cost to manufacture each laptop, its label as "illegal" or "legal",
+# and the number of legal laptops to be manufactured each day,
+# find the maximum cost incurred by the company in a single day in manufacturing all the laptops.
+
+# Note that if a laptop is labeled as illegal, its manufacturing cost is still incurred by the company,
+# even though it is not included in the laptop count.
+# Also, days are only taken into when the daily laptop count has been completely met.
+# If there are no such days, the answer is 0.
+
+# For example,
+# let's say there are n = 5 laptops, where cost = [2,5, 3, 11, 1].
+# The labels for these laptops are labels = ["legal", "illegal", "legal", "illegal", "legal").
+#     Finally, the daily Count = 2, which means that the company needs to manufacture 2 legal laptops each day.
+# The queue of laptops can be more easily viewed as follows:
+
+# cost 2, "legal" 
+# cost 5, "illegal" 
+# cost 3, "legal" 
+# cost 11, "illegal" 
+# cost 1, "legal"
+
+# On the first day, the first three laptops are manufactured in order to reach the daily count of 2 legal laptops.
+# The cost incurred on this day is 2 + 5 + 3 = 10.
+# On the second day, the fourth and fifth laptops are manufactured,
+# but because only one of them is legal, the daily count isn't met, so that day is not taken into consideration.
+# Therefore, the maximum cost incurred on a single day is 10.
+
+
+# My Algorithm:
+# Since the laptops and labels are always in pairs, so I loop over them using index-based loop.
+# To get an easier loop control, I use while-loop whose conditional logic is more flexible than for-loop.
+
+# I will check the laptop one by one, each check will add up the "cost count" incrementally (line10).
+# Only if there is any laptop has label "legal", the "legal" counter will add up +1 (line9).
+# Repeat it until the legal counter equals to DailyCount.
+
+# I need to add a guard clause (line12), since at the last checking day the number of legals might be below DailyCount.
+# So, only if "legal" counter is exactly the same as DailyCount, append the dailycost into the final list (total_cost).
+# If there are still laptops left, then reset the "legal" counter and "dailycost" counter, repeat the checking process.
+
+# The final list (line3) must have at least 0, in case none of the laptops meet the checking criteria.
+# Return the maximum value of the final list.
