04cd0495c5fdcf05eea6e3b22d433d6a4fb77152
Venska Arman Nur Rosyidin <armanvenska@gmail.com>
2023-05-15 11:18:34 +0700

    optimize code

--- HR_Tests/Test1/N3_Unique_Char.py
+++ HR_Tests/Test1/N3_Unique_Char.py
@@ -6,6 +6,17 @@ def getUniqueCharacter(s): # 15/15 passed
     if len(f) ==0:
         return -1
     else:
-        return min(f)
+        return f[0]
+print(getUniqueCharacter("statisatics"))
+print(getUniqueCharacter("statistics"))
+print(getUniqueCharacter("statitic"))
 
-getUniqueCharacter("sctatisatics")
+def guc(s): # 15/15 passed
+    for c in s:
+        if s.count(c) == 1: return s.index(c) +1
+    return -1
+
+# Diberikan sebuah string yang memuat huruf-huruf
+# Jika ada karakter unik, return nomor urutan karakter tersebut
+# apabila ada lebih dari satu karakter unik, return nomor urut terkecil
+# Jika tidak ada karakter unik, return -1
