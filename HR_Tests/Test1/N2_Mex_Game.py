ba19538e84573db955c09adc9dc002be57516c65
Venska Arman Nur Rosyidin <armanvenska@gmail.com>
2023-05-19 13:28:55 +0700

    modify HR Test1 answers

--- HR_Tests/Test1/N2_Mex_Game.py
+++ HR_Tests/Test1/N2_Mex_Game.py
@@ -13,30 +13,6 @@ def getMaximumMEX(arr):
             arr[idx]=x
     return mex, arr
 
-# print(getMaximumMEX([1,0,2,3,5])) #output 4
+print(getMaximumMEX([1,0,2,3,5])) #output 4
 # print(getMaximumMEX([3,1,2,2])) # output 3
 # print(getMaximumMEX([2,0,0])) # output 2
-
-def gm(arr):
-    arr.sort()
-    uni=sorted(list(set(arr)))
-    mex=[]
-    n=len(arr)
-    temp = []
-    for x in range(n+1):
-        if x not in arr:
-            temp.append(x)
-    mex.append(min(temp))
-    for num in uni:
-        if arr.count(num)>1:
-            idx = arr.index(num)
-            arr[idx] = mex[0]
-    print(arr, uni, mex)
-    temp = []
-    for x in range(n+1):
-        if x not in arr:
-            temp.append(x)
-    mex.append(min(temp))
-    print(arr, uni, mex)
-    # return mex
-print(gm([3,1,2,2]))
