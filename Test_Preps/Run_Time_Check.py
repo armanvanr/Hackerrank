ecfb218e62976a81cd41687e28cb7762c159db2a
Venska Arman Nur Rosyidin <armanvenska@gmail.com>
2023-05-21 09:32:07 +0700

    update

--- Test_Preps/Run_Time_Check.py
+++ Test_Preps/Run_Time_Check.py
@@ -6,9 +6,12 @@ sys.path.append('A:\\Makers Institute\\Codes\\Week 2\\Hackerrank\\HR_Tests\\Test
 
 from N1_K_Subarrays import ksub1, ksub2, ksub3
 from N2 import gainMaxValue
+from N6 import get_max_mex4, get_max_mex2
 
 arr1 = [5, 10, 11, 9, 5]
 k1 = 2
+
+array = [1200]*5000+[5]*80000+[70000]*2000+[0]*8000
 
 # CODE 1
 start1 = time.time()
@@ -26,7 +29,8 @@ print(ksub3(k1, arr1))
 end3 = time.time()
 
 start4 = time.time()
-gainMaxValue([2, 5, -8, -6, -7], 3)
+# gainMaxValue([2, 5, -8, -6, -7], 3)
+get_max_mex2(array)
 end4 = time.time()
 # print the difference between start
 # and end time in milli. secs
